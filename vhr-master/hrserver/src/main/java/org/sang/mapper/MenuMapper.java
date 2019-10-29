a170b6eff47ed5f222a95fa3bea6d3715cb87c4b
zengjx <1164512033@qq.com>
2019-10-29 10:09:33 +0800

    第一次提交  2019.10.29

--- vhr-master/hrserver/src/main/java/org/sang/mapper/MenuMapper.java
+++ vhr-master/hrserver/src/main/java/org/sang/mapper/MenuMapper.java
@@ -8,11 +8,11 @@ import java.util.List;
  * Created by sang on 2017/12/28.
  */
 public interface MenuMapper {
-    List<Menu> getAllMenu();
+    List<Menu> getAllMenu();//获取所有菜单
 
-    List<Menu> getMenusByHrId(Long hrId);
+    List<Menu> getMenusByHrId(Long hrId);//通过hr  id  查询菜单
 
-    List<Menu> menuTree();
+    List<Menu> menuTree();//
 
     List<Long> getMenusByRid(Long rid);
 }
